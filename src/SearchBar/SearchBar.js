69f8e5543fc11fc78b9f1e5673c00749dbc8b08f
Seyed Mohammad Ali Fateminia <mfateminia@gmail.com>
2018-11-12 22:45:06 +0100

    searchbar added, background added bootstrap added

---
+++ src/SearchBar/SearchBar.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import './SearchBar.css';
+
+class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  };
+    }
+    render() {
+        return (
+            <input type = 'text'/>
+        );
+    }
+}
+
+export default SearchBar;
